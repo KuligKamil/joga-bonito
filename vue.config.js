2b70eb368097cbdd9ad73b275ea450d2ab79432d
KuligKamil <kamil.marek.kulig@gmail.com>
2022-01-18 01:23:51 +0100

    chore: add proxy to localhost 8000

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+    transpileDependencies: [
+        'vuetify'
+    ],
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                pathRewrite: {'^/api' : ''},
+                // rewrite:(path) => path.replace('api/', ''), Vite
+                secure: false,
+                logLevel: 'debug'
+            },
+        },
+        host: 'localhost'
+    }
 }
